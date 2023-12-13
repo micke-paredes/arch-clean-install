0a850b7cb1bb8cdc73b2e1bde75166937871c67c
DeveloPolaroid <miguelttech.19@gmail.com>
2023-12-13 00:35:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-sudo pacman -S lsd picom feh xdotool net-tools xclip maim polybar git bat zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S lsd picom feh xdotool net-tools \
+        xclip maim polybar git bat zsh-autosuggestions zsh-syntax-highlighting \ 
+        nautilus  neofetch rofi ranger cmus spotify-launcher playerctl imagemagick \
+        kitty vscode
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
