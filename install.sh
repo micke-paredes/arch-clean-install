a968a0d165f31eaaac33210d098c215130b5deaa
DeveloPolaroid <miguelttech.19@gmail.com>
2023-12-13 00:39:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 sudo pacman -S lsd picom feh xdotool net-tools \
         xclip maim polybar git bat zsh-autosuggestions zsh-syntax-highlighting \ 
         nautilus  neofetch rofi ranger cmus spotify-launcher playerctl imagemagick \
-        kitty vscode
+        kitty vscode locate
 
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git /usr/share/plugins/zsh-autocomplete
