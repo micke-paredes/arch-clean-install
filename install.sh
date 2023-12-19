fb37b7287484ce60343380f78c1e3bf1f2188439
DeveloPolaroid <miguelttech.19@gmail.com>
2023-12-18 18:38:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ sudo pacman -S lsd picom feh xdotool net-tools \
         kitty vscode locate nerd-fonts thunderbird dunst
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/plugins/fast-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git /usr/share/plugins/zsh-autocomplete
+
+paru -S pulseaudio pulseaudio-ctl ttf-icomoon-feather
